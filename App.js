d57ef5c73013127d99f7d4d98693121253b35b4c
MdAshiqurRahmanZayed <mdzayed2019@gmail.com>
2024-05-01 15:21:06 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MainComponent from './src/MainComponent';
+import { Provider } from 'react-redux';
+import Store from './src/redux/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={Store}>
+      <MainComponent />
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
